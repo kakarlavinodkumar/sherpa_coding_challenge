4ef9744a2aaccde33f606665392b25353977d4d8
vinod kumar kakarla <vinodkakarla564@gmail.com>
2025-04-20 18:17:08 -0700

    feat(database): implement MongoDB connection with Flight schema

--- src/database.ts
+++ src/database.ts
@@ -1,3 +1,4 @@
+import mongoose from "mongoose"
 import {join} from "path"
 import {Database} from "sqlite3"
 
@@ -12,4 +13,34 @@ export const getDBConnection = (): Database => {
         db = new sqlite3.Database(dbLocation)
     }
     return db
+}
+
+export const getMongoDBConnection = (): mongoose.Connection => {
+
+    const uri = process.env.DB_URI || 'mongodb://localhost:27017/local_db';
+    
+    const FlightSchema = new mongoose.Schema({
+        flightNumber: { type: String, required: true },
+        airline: { type: String, required: true },
+        departure: { type: String, required: true },
+        destination: { type: String, required: true },
+        departureTime: { type: Date, required: true },
+        arrivalTime: { type: Date, required: true },
+    });
+
+    if(!mongoose.models.Flight) {
+        mongoose.model('Flight', FlightSchema);
+    }
+
+    mongoose.connect(uri)
+        .then(() => 
+        {
+            console.log('MongoDB connection established successfully');
+        })
+        .catch((err: any) => {
+            console.error('Error connecting to MongoDB:', err);
+            process.exit(1); // Exit the process if connection fails
+        });
+    
+    return mongoose.connection;
 }
