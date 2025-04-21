a5d933552aeda36d83eaac675259243eea1e9345
vinod kumar kakarla <vinodkakarla564@gmail.com>
2025-04-20 18:17:47 -0700

    feat(routes): add flight routes to main router

--- src/server/route.ts
+++ src/server/route.ts
@@ -1,12 +1,15 @@
 //Master routing file to route all the api's
 import express from "express"
 import eventRoute from "./event/api/route"
+import flightRoutes from "./flight/api/route";
+
 const router = express.Router();
 
 //Authorization check for event api's
 // app.use("/event", isAuthorized)
 // routing for event api's
 router.use("/events", eventRoute);
+router.use("/flights", flightRoutes)
 // router.use("/another_route", anotherRoute); 
 
 export default router;
