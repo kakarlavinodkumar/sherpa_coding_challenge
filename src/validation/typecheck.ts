281b4786c67b7f3fd7f2ce38d5007e448a0186a1
vinod kumar kakarla <vinodkakarla564@gmail.com>
2025-04-20 18:20:52 -0700

    fix(validation): correct NaN check in typeCheckService function

--- src/validation/typecheck.ts
+++ src/validation/typecheck.ts
@@ -1,7 +1,7 @@
 export const typeCheckService = function (value: any, expected_type: string) {
     try {
         if(expected_type === "number") {
-            if(Number(value) === NaN) {
+            if(isNaN(Number(value))) {
                 throw `${value} should be of type ${expected_type}`;
             } else {
                 return
