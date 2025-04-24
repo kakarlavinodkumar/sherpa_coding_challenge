5727b5a07099ce5d476357b538935924b5e5410a
vinod kumar kakarla <vinodkakarla564@gmail.com>
2025-04-24 16:23:47 -0700

    fix(route): remove azureEventBusRoutes from main router

--- src/server/route.ts
+++ src/server/route.ts
@@ -4,18 +4,17 @@ import eventRoute from "./event/api/route"
 import flightRoutes from "./flight/api/route";
 import messageRoutes from "./message/api/route";
 import flightEventRoutes from "./flightevent/api/route";
-import azureEventBusRoutes from "./azureeventbus/api/route";
 
 const router = express.Router();
 
 //Authorization check for event api's
 // app.use("/event", isAuthorized)
+
 // routing for event api's
 router.use("/events", eventRoute);
 router.use("/flights", flightRoutes);
 router.use("/messages", messageRoutes);
 router.use("/flightevents", flightEventRoutes);
-router.use("/azureeventbus", azureEventBusRoutes);
 // router.use("/another_route", anotherRoute); 
 
 export default router;
