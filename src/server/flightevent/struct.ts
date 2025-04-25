2afea2461ef34544fee680d722e656a7934d509f
vinod kumar kakarla <vinodkakarla564@gmail.com>
2025-04-24 22:02:34 -0700

    feat(struct): add GetFlightEventsByFilterPayload interface for filtering events

--- src/server/flightevent/struct.ts
+++ src/server/flightevent/struct.ts
@@ -27,6 +27,16 @@ export interface DeleteFlightEventPayload {
     _id: string
 }
 
+export interface GetFlightEventsByFilterPayload {
+    flight_number: string,
+    departure_from: string,
+    arrival_to: string,
+    local_departure_date_time: string,
+    local_arrival_date_time: string,
+    event_type: string,
+    event_name: string,
+    event_time: string
+}
 export interface CreateFlightEventResponse {
     _id: string // _id of the created flight event
 }
