271b42898f14af6eb8b4554750d9393bc889a7b8
vinod kumar kakarla <vinodkakarla564@gmail.com>
2025-04-24 22:02:24 -0700

    feat(azureeventbus): integrate GetFlightEventByFiltersService and handle FLIGHT_EVENT_STATUS_PAUSED

--- src/server/azureeventbus/service/azureeventbuservice.ts
+++ src/server/azureeventbus/service/azureeventbuservice.ts
@@ -1,6 +1,8 @@
 const { ServiceBusClient } = require("@azure/service-bus");
 
+import { FLIGHT_EVENT_STATUS_PAUSED } from "../../../appconstants/constants";
 import { config } from "../../../config/config";
+import { GetFlightEventByFiltersService } from "../../flightevent/service/flighteventservice";
 
 export const FlightEventsConsumer = () => {
     try {
@@ -14,6 +16,11 @@ export const FlightEventsConsumer = () => {
         receiver.subscribe({
             processMessage: async (message: any) => {
                 console.log("Processing message:", message.body);
+                const flight_event =  await GetFlightEventByFiltersService(message.body);
+                if(flight_event && flight_event.status == FLIGHT_EVENT_STATUS_PAUSED) {
+                    
+                }
+                console.log("flight_event : ", flight_event);
                 await receiver.completeMessage(message);
             },
             processError: async (err: any) => {
